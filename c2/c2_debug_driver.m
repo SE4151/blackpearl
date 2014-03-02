7dcf713b9a0d5eeeb88ab2a49742eef20817a9d3
Thomas Wang <twang@nps.edu>
2014-03-02 13:28:02 -0800

    C2 Implemented Version 1.0
    - RADAR simulator is incomplete
    - EO auto target recognition not implemented
    
    Signed-off-by: Thomas Wang <twang@nps.edu>

--- c2/c2_debug_driver.m
+++ c2/c2_debug_driver.m
@@ -9,8 +9,7 @@ p8_speed = 1/10; % pixel/sec
 h60_speed = 1/30; % pixel/sec
 pirate_speed = 1/120; % pixel/sec
 launch_pos = [577 406];
-search_waypoints = [297 628 646 582 464; ...
-                    405 323 524 675 840];
+search_waypoints = [297 628; 405 323 ];
 search_waypoints = search_waypoints';
 p8_pos = launch_pos;
 h60_pos = launch_pos;
@@ -19,7 +18,7 @@ h60_waypoint = p8_waypoint;
 
 
 dt = 100; % time step
-for t = 0:dt:5000
+for t = 0:dt:10000
     % Generate environment map
     [global_map, eo_map, boarded, pirate_pos] = Environment(t);
 
@@ -36,19 +35,38 @@ for t = 0:dt:5000
     else
         h60_pos = h60_pos + (h60_waypoint-h60_pos)./norm(h60_waypoint-h60_pos).*h60_speed*dt;
     end
+    flight_output.p8_position = p8_pos;
+    flight_output.h60_position = h60_pos;
 
     % RADAR
     % check to make sure pirate is in radar view and dwell time is long enough
     % for track generation
+    if((pirate_pos(1) >= p8_pos(1)-100 & pirate_pos(1) <= p8_pos(1)+100) & ...
+        (pirate_pos(2) >= p8_pos(2)-100 & pirate_pos(2) <= p8_pos(2)+100))
+        % pirate in radar field of view
+
+    else
+
+    end
+    if(t == 0)
+        last_pirate_pos = pirate_pos(1:2);
+    end
+    radar_output(1).position = pirate_pos(1:2);
+    radar_output(1).velocity = (pirate_pos(1:2) - last_pirate_pos)/dt;
+    last_pirate_pos = pirate_pos(1:2);
 
     % EO
     % Generate EO image, take the 50 x 50 from eo_map
     h60_grid = round(h60_pos);
     eo_image = eo_map(h60_grid(2)-25:h60_grid(2)+25, h60_grid(1)-25:h60_grid(1)+25);
+    eo_output.valid_target = 0;
 
     % C2
-    p8_waypoint = pirate_pos(1:2);
-    h60_waypoint = p8_waypoint;
+%    p8_waypoint = pirate_pos(1:2);
+%    h60_waypoint = p8_waypoint;
+    c2_output = c2_main(t, dt, radar_output, eo_output, flight_output);
+    p8_waypoint = c2_output.p8_waypoint;
+    h60_waypoint = c2_output.h60_waypoint;
 
     % Display scenario
     figure(999);
@@ -57,17 +75,54 @@ for t = 0:dt:5000
     hold on,
     plot(pirate_pos(1), pirate_pos(2), 'rs', ...
         p8_pos(1), p8_pos(2), 'bs', ...
-        h60_pos(1), h60_pos(2), 'gs');
+        h60_pos(1), h60_pos(2), 'go');
+    %legend('Pirate', 'P8', 'H60');
     plot(search_waypoints(:, 1), search_waypoints(:, 2), 'gs-');
     rect = [p8_pos(1)-100 p8_pos(2)-100, 200, 200];
     rectangle('Position', rect, 'EdgeColor', 'b');
-    axis on;
+    text(20, 580, sprintf('t = %.0f', t), 'color', 'r', 'FontSize', 16, 'BackgroundColor', 'w');
+    axis off;
     hold off;
 
     figure(9);
     imshow(eo_image);
-    pause(0.1);
+    set(gcf, 'Name', 'EO Image');
+%    pause(0.1);
 end
+
+
+%%
+map = imread('africa_map.png');
+
+search_waypoints = [297 628 646 582 464; ...
+                    405 323 524 675 840];
+search_waypoints = search_waypoints';
+
+figure(1);
+imshow(map);
+colormap('gray')
+
+hold on;
+plot(search_waypoints(:, 1), search_waypoints(:, 2), 'gs-');
+hold off;
+
+
+axis image;
+hold on;
+id = 1;
+rect = [search_waypoints(id, 1)-100 search_waypoints(id, 2)-100, 200, 200];
+rectangle('Position', rect, 'EdgeColor', 'r');
+hold off;
+
+%%
+figure(2);
+mask2 = im2bw(mask, graythresh(mask));
+bwtraceboundary(mask2, [681 337], 'N', 4);
+bound = bwboundaries(mask);
+bound = cell2mat(bound);
+plot(bound(:, 1), bound(:, 2));
+%%
+
 
 
 %%
--- c2/c2_main.m
+++ c2/c2_main.m
@@ -1,27 +1,127 @@
-function [c2_output] = c2_main(radar_input, eo_input, flight_input)
+function [c2_output] = c2_main(time, dt, radar_input, eo_input, flight_input)
 % The entry point for C2 sub-system.
 %
+if(nargin==0)
+   disp('BLACK PEARL - Command and Control System 1.0');
+   return;
+end
 
 %% Expected input
+% From LSI
+% time = 0; % simulation time (starting from 0, in seconds)
+% dt = 60; % simulatino time step (seconds/step)
+
 % From the radar
-time = 0; % the time in seconds
-radar_track(1).position = [0 0]; % position of a track in global (x, y) coordinate, unit: pixel
-radar_track(1).velocity = [0 0]; % velocity of a track in global (x, y) coordinate, unit: pixel/second
-% you can have more tracks
+% radar_track(1).position = [0 0]; % position of a track in global (x, y) coordinate, unit: pixel
+% radar_track(1).velocity = [0 0]; % velocity of a track in global (x, y) coordinate, unit: pixel/second
+% % you can have more tracks
+%
+% % From the EO
+% eo_input.valid_target = logical(false); % human validated valid target declaration
+%
+% % From Flight
+% flight_input.p8_position = [0 0]; % position of p8 in global (x, y) coordinate, unit: pixel
+% flight_input.h60_position = [0 0]; % position of h60 in global (x, y) coordinate, unit: pixel
 
-% From the EO
-time = 0; % the time in seconds
-valid_target = logical(false); % human validated valid target declaration
-eo_image = zeros(50, 50); % the 50 by 50 gray scale EO image
-
-% From Flight
-time = 0; % the time in seconds
-p8_position = [0 0]; % position of p8 in global (x, y) coordinate, unit: pixel
-h60_position = [0 0]; % position of h60 in global (x, y) coordinate, unit: pixel
+%%
+radar_track = radar_input;
+valid_target = eo_input.valid_target;
+p8_position = flight_input.p8_position;
+h60_position = flight_input.h60_position;
 
 %% C2 implemenation
-disp('You are in c2_main().');
+
+% A2.1 Analyze rdar data
+% Compare speed of track
+pirate_speed = 1/120;
+pirate_pos = [];
+for k = 1:length(radar_input)
+   speed = norm(radar_track(k).velocity);
+   % Determine ship type
+   if(speed > pirate_speed * 0.9 || speed < pirate_speed * 1.1)
+       % Determine pirate location
+       pirate_pos = radar_track(k).position;
+       pirate_vel = radar_track(k).velocity;
+   end
+end
+
+% A2.2 Generate valid target flag
+% This happens in the end
+
+% A2.3 Direct H60
+
+% A2.3.1 Determine H60 Flight Strategy
+if(isempty(pirate_pos)) % No pirate found
+    h60_strategy = 'search';
+
+else
+    h60_strategy = 'intercept';
+end
+
+% A2.3.2 Calculate H60 Waypoint
+switch h60_strategy
+    case 'search'
+        h60_waypoint = p8_position; % H60 will follow P8.
+        % maybe it shouldn't go too far north
+
+    case 'intercept'
+        if(norm(pirate_vel) == 0 )
+            h60_waypoint = pirate_pos; % simple tail chase
+        else
+            % we can do better with proportional navigation
+            % predict where the pirate will be in the next time step
+            pirate_speed = 1/120; % pixel/sec
+            next_pirate_pos = pirate_pos + pirate_vel/norm(pirate_vel)*pirate_speed*dt;
+            h60_waypoint = next_pirate_pos;
+        end
+    otherwise
+        warning('Undefined H60 strategy!');
+end
+
+% A2.4 Direct P8
+
+% A2.4.1
+if(isempty(pirate_pos)) % No pirate found
+    p8_strategy = 'search';
+else
+    p8_strategy = 'follow';
+end
+
+% A2.4.2 Calculate H60 Waypoint
+search_waypoints = [297 628 646 582 464; ...
+                    405 323 524 675 840];
+search_waypoints = search_waypoints';
+persistent last_waypoint;
+if(time == 0)
+    last_waypoint = search_waypoints(1, :);
+end
+switch p8_strategy
+    case 'search'
+        % Need to know the current position
+        d1 = norm(search_waypoints(1, :) - p8_position);
+        d2 = norm(search_waypoints(2, :) - pi_position);
+        if(d1 < 2)
+            p8_waypoint = search_waypoints(2, :);
+        elseif(d2 < 2)
+            p8_waypoint = search_waypoints(1, :);
+        else
+            p8_waypoint = last_waypoint;
+        end
+    case 'follow'
+        pirate_speed = 1/120; % pixel/sec
+        if(norm(pirate_vel) == 0)
+           next_pirate_pos = pirate_pos;
+        else
+            next_pirate_pos = pirate_pos + pirate_vel/norm(pirate_vel)*pirate_speed*dt;
+        end
+        p8_waypoint = next_pirate_pos;
+    otherwise
+        warning('Undefined P8 strategy!');
+end
 
 
 %% The output
 c2_output = C2_Output_Interface; % The outputs are defined in 'C2_Output_Interface.m'
+c2_output.valid_target = valid_target;
+c2_output.p8_waypoint = p8_waypoint;
+c2_output.h60_waypoint = h60_waypoint;
